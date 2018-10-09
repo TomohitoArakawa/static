7196bf82cfefcaee6b85bc050fad0b2ddfaa7c75
Tomohito Arakawa <u.jin.arakawa@gmail.com>
2018-10-10 06:50:04 +0900

    create build task for css sprite image

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ const minify = require('gulp-clean-css');
 const cssLint = require('gulp-csslint');
 const rename = require('gulp-rename');
 const sourcemap = require('gulp-sourcemaps');
-const cssSprite = require('gulp.spritesmith');
+const sprite = require('gulp.spritesmith');
 const browserSync = require('browser-sync');
 const runSequence = require('run-sequence');
 
@@ -41,11 +41,28 @@ gulp.task('sass', () => {
     browsers: ['last 2 version', 'iOS >= 8.1' , 'Android >= 4.4'],
     cascade: false
   }))
-  .pipe(minify())
+  // .pipe(minify())
   .pipe(rename({
     extname: '.min.css'
   }))
   .pipe(gulp.dest('./'))
+});
+
+gulp.task('sprite', () => {
+    let spriteData = gulp.src('./img/sprite/*.png')
+        .pipe(sprite({
+            imgName: 'ico_service_sprite.png',                        // スプライト画像
+            cssName: '_ico_service_sprite.scss',                      // 生成される CSS テンプレート
+            imgPath: './img/ico_service_sprite.png', // 生成される CSS テンプレートに記載されるスプライト画像パス
+            cssFormat: 'scss',                            // フォーマット拡張子
+            cssVarMap: (sprite) => {
+                sprite.name = "service-icon_" + sprite.name;      // 生成される CSS テンプレートに変数の一覧を記述
+            }
+    }));
+    spriteData.img
+        .pipe(gulp.dest('./img'));     // imgName で指定したスプライト画像の保存先
+    return spriteData.css
+        .pipe(gulp.dest('./scss'));       // cssName で指定した CSS テンプレートの保存先
 });
 
 gulp.task('default', ['browserSync' , 'watch' , 'sass'], () => {
@@ -56,6 +73,6 @@ gulp.task('test', ['cssLint'], () => {
 
 });
 
-gulp.task('build', ['cssSprite'], () => {
+gulp.task('build', ['sprite'], () => {
 
 });
